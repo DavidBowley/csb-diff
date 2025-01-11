e98041b56dda9969965962cbb69bb4431ba47f58
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-01-11 15:54:03 +0000

    Loading from XML file and parsing with Cheerio

--- csb-diff.ts
+++ csb-diff.ts
@@ -1,12 +1,20 @@
-import axios from 'axios';
+import * as cheerio from 'cheerio';
 
-axios.get('http://www.bbc.co.uk')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
-    .finally(function() {
-        console.log('This bit of code is always executed at the end');
-    })
+import fs from 'node:fs';
+import path from 'node:path';
+
+function findInXML() {
+    // Will rename later but for now it's a proof of concept for using Cheerio to find specific sections of the XML file
+    const bookXMLPath = path.join(__dirname, 'data', 'US', '45-Rom.xml');
+    let bookXML: string;
+    try {
+        bookXML = fs.readFileSync(bookXMLPath, 'utf8');
+    } catch (err) {
+        console.error(err);
+        return
+    }
+    const $ = cheerio.load(bookXML, {xml: true});
+    console.log($.text());
+}
+
+findInXML();
