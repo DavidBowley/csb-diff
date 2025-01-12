525b87ce76872304948e9166d03a164eee918d37
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-01-12 19:18:16 +0000

    Added logic for normalising the quotation marks - currently WIP for possessive nouns

--- csb-diff.ts
+++ csb-diff.ts
@@ -22,12 +22,18 @@ function findInXML(version: string, bookFilename:string): void {
     console.log('\nCSB Version: ' + version);
     console.log('Book: ' + bookFilename);
     $chapters.each((i, element) => {
-        if (i > 0) {
-            return;
+        // TODO: Remove if statement once debugging complete
+        if (i !== 6) {
+            // return;
         }
-        console.log('Chapter: ' + (i+1));
+        // TODO: Remove extra linebreaks once finished debugging as the final page will use CSS to handle paragraph spacing
+        console.log('\n\nChapter: ' + (i+1));
         const paragraphs = $(element).find('p');
         paragraphs.each((i, element) => {
+            // TODO: Remove if statement once debugging complete
+            if (i !== 6) {
+                // return;
+            }
             let paragraph = $(element).text();
             // Remove all whitespace longer than a single character in length
             // For US XML: handles large amounts of seemingly random whitespace
@@ -35,12 +41,56 @@ function findInXML(version: string, bookFilename:string): void {
             paragraph = paragraph.replace(/\s+/g, " ");
             // UK XML uses en-dash vs. US em-dash which causes false positives in the diff 
             paragraph = paragraph.replace(/\u2013/g, "\u2014");
-            console.log(paragraph);
+            // TODO: Remove extra linebreak once finished debugging as the final page will use CSS to handle paragraph spacing
+            // Add back in when returning to normal...
+            // console.log('\n' + paragraph);
+            
+            if (version === 'UK') {
+                // Logic for swapping single and double curly quotes - note this must occur AFTER replacing <sup> and fixing spaces
+                // or else there can be false positives in the apostrophe search below
+                let str = '';                
+                for (let i = 0; i < paragraph.length; i++) {
+                    const char = paragraph[i];
+                    // Swap open-single quote for open-double quote
+                    if (char === '\u2018') {
+                        str += '\u201C';
+                    }
+                    // Swap open-double quote for open-single quote
+                    else if (char === '\u201C') {
+                        str += '\u2018';
+                    }
+                    // Swap close-single quote for close-double quote
+                    else if (char === '\u2019') {
+                        // WIP: Detecting if apostrophe or close-single quote
+                        // Currently checks if next character is an alpha character which works in lots of cases
+                        // However doesn't work for posessives on words ending with 's', e.g. it turns ' into ", e.g. Vipers" (Rom 3:13)
+                        // Likely can't be fixed with RegEx as requires lots of language-specific context                   
+                        if (/^[a-zA-Z]$/.test(paragraph[i+1])) {
+                            str += char;
+                        }
+                        else {
+                            str += '\u201D';
+                        }
+                    }
+                    // Swap close-double quote for close-single quote
+                    else if (char === '\u201D') {
+                        str += '\u2019';
+                    }
+                    // Else retain original character
+                    else {
+                        str += char;
+                    }
+                }
+                paragraph = str;
+            }
+            
+            console.log('\n' + paragraph);
+                
+            }
             // return false;
+        )
         }
     )
-    });
-
 }
 
 
