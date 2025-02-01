ca2d2454420fcd54f4bdb12d1cb138004be2fbb0
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-02-01 17:54:30 +0000

    Proof of concept for fetching JSON file and converting to DOM objects for output

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,28 @@
-console.log('test');
+"use strict";
+function fetchJson() {
+    fetch("./data/test45.json")
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+    })
+        .then((data) => { debugInsertDiff(data); })
+        .catch((error) => {
+        throw new Error(`Error: ${error.message}`);
+    });
+}
+function debugInsertDiff(book) {
+    const diffContainer = document.getElementById('test-romans');
+    const dFrag = document.createDocumentFragment();
+    for (const chapter of book) {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = '<div>' + chapter + '</div>';
+        if (wrapper.firstChild) {
+            dFrag.append(wrapper.firstChild);
+        }
+    }
+    console.log(dFrag);
+    diffContainer === null || diffContainer === void 0 ? void 0 : diffContainer.appendChild(dFrag);
+}
+fetchJson();
--- frontend/index.ts
+++ frontend/index.ts
@@ -1,1 +1,34 @@
-console.log('test');
+function fetchJson() {
+    fetch("./data/test45.json")
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((data) => {debugInsertDiff(data)} )
+    .catch((error) => {
+        throw new Error(`Error: ${error.message}`);
+    });
+}
+
+function debugInsertDiff(book: string[]) {
+    const diffContainer = document.getElementById('test-romans');
+    const dFrag = document.createDocumentFragment();
+    for (const chapter of book) {
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = '<div>' + chapter + '</div>';
+        if (wrapper.firstChild) {
+            dFrag.append(wrapper.firstChild);
+        }
+    }
+    console.log(dFrag);
+    diffContainer?.appendChild(dFrag);
+    
+    
+}
+
+fetchJson();
+
+
+
