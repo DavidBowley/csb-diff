5d7a84f786b2a2443868b1c4c275bc8d697168e5
David Bowley <46280190+DavidBowley@users.noreply.github.com>
2025-02-21 17:56:43 +0000

    Added previous/next chapter navigation buttons (only top ones currently working)

--- frontend/index.js
+++ frontend/index.js
@@ -30,21 +30,44 @@ function populateBookSelect() {
     });
 }
 function updateChapter(book, chapter) {
+    // Expects a human-readable chapter number from the caller, 
+    // which is immediately reset for the 0-indexed array
     chapter -= 1;
     const diffContainer = document.getElementById('diff-container');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
+    const prevChapterBtnTop = document.getElementById('nav-ctrl-prev-chapter-btn-top');
     if (diffContainer) {
         diffContainer.innerHTML = book[chapter];
+    }
+    // Set disabled state of next/previous chapter buttons depending on what chapter we're on
+    console.log(`Current chapter index: ${chapter}`);
+    console.log(`Is next chapter in the book array? ... ${chapter + 1 in book}`);
+    console.log(`Is previous chapter in the book array? ... ${chapter - 1 in book}`);
+    if (chapter + 1 in book) {
+        nextChapterBtnTop === null || nextChapterBtnTop === void 0 ? void 0 : nextChapterBtnTop.removeAttribute('disabled');
+    }
+    else {
+        nextChapterBtnTop === null || nextChapterBtnTop === void 0 ? void 0 : nextChapterBtnTop.setAttribute('disabled', '');
+    }
+    if (chapter - 1 in book) {
+        prevChapterBtnTop === null || prevChapterBtnTop === void 0 ? void 0 : prevChapterBtnTop.removeAttribute('disabled');
+    }
+    else {
+        prevChapterBtnTop === null || prevChapterBtnTop === void 0 ? void 0 : prevChapterBtnTop.setAttribute('disabled', '');
     }
 }
 function openBook(book) {
     // Updates the GUI to the selected book, including resetting chapter <select>'s child <option>s
+    // and enabling the relevant prev/next chapter controls
     // Defaults to showing chapter 1
     const chapterSelect = document.getElementById('nav-ctrl-chapter');
     const navChapterSubmit = document.getElementById('nav-ctrl-chapter-submit');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
     chapterSelect.innerHTML = '';
     for (let i = 0; i < book.length; i++) {
         const option = document.createElement('option');
         option.appendChild(document.createTextNode(String(i + 1)));
+        option.setAttribute('value', String(i + 1));
         chapterSelect === null || chapterSelect === void 0 ? void 0 : chapterSelect.appendChild(option);
     }
     // Re-enable the chapter picker and submit button now it contains valid values
@@ -62,6 +85,8 @@ function openBook(book) {
     const chapterSelect = document.getElementById('nav-ctrl-chapter');
     const navBookSubmit = document.getElementById('nav-ctrl-book-submit');
     const navChapterSubmit = document.getElementById('nav-ctrl-chapter-submit');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
+    const prevChapterBtnTop = document.getElementById('nav-ctrl-prev-chapter-btn-top');
     // Pull in book names and setup <select> with option values that correspond to JSON filenames
     // and bibleBook top-level array indicies
     populateBookSelect();
@@ -69,7 +94,7 @@ function openBook(book) {
         const bookRef = Number(bookSelect.value);
         // If we haven't yet fetched the JSON file and stored internally, then do so...
         if (bibleBook[bookRef] === null) {
-            console.log(`bibleBook array at item ${bookRef} is null. Will download the relevant JSON file and store internally.`);
+            // console.log(`bibleBook array at item ${bookRef} is null. Will download the relevant JSON file and store internally.`)
             fetchJson(bookRef + '.json')
                 .then((book) => {
                 openBook(book);
@@ -81,7 +106,7 @@ function openBook(book) {
         }
         // else we have that book already stored in memory that we can use
         else {
-            console.log(`bibleBook array at item ${bookRef} already contains our JSON object.`);
+            // console.log(`bibleBook array at item ${bookRef} already contains our JSON object.`);
             openBook(bibleBook[bookRef]);
         }
     });
@@ -92,16 +117,40 @@ function openBook(book) {
             alert('Could not find that chapter - make sure you have pressed the Open Book button first');
         }
         else {
-            console.log(`Chapter: ${chapter}, bookRef: ${bookRef}`);
-            console.log('book.length: ' + bibleBook[bookRef].length);
+            // console.log(`Chapter: ${chapter}, bookRef: ${bookRef}`)
+            // console.log('book.length: ' + bibleBook[bookRef].length)
             updateChapter(bibleBook[bookRef], chapter);
         }
     });
     bookSelect.addEventListener('change', () => {
         // Once the user updates the book <select> there's no way to guarantee we won't get index
         // out-of-range issues on the chapter picker, so it should be disabled until the new book
-        // is loaded
+        // is loaded. Same logic applies to all chapter navigation buttons.
         chapterSelect.setAttribute('disabled', '');
         navChapterSubmit === null || navChapterSubmit === void 0 ? void 0 : navChapterSubmit.setAttribute('disabled', '');
+        nextChapterBtnTop === null || nextChapterBtnTop === void 0 ? void 0 : nextChapterBtnTop.setAttribute('disabled', '');
+        prevChapterBtnTop === null || prevChapterBtnTop === void 0 ? void 0 : prevChapterBtnTop.setAttribute('disabled', '');
+    });
+    nextChapterBtnTop === null || nextChapterBtnTop === void 0 ? void 0 : nextChapterBtnTop.addEventListener('click', () => {
+        const chapter = Number(chapterSelect.value);
+        const bookRef = Number(bookSelect.value);
+        if (bibleBook[bookRef] === null) {
+            alert('Could not find that chapter - make sure you have pressed the Open Book button first');
+        }
+        else {
+            updateChapter(bibleBook[bookRef], chapter + 1);
+            chapterSelect.value = String(chapter + 1);
+        }
+    });
+    prevChapterBtnTop === null || prevChapterBtnTop === void 0 ? void 0 : prevChapterBtnTop.addEventListener('click', () => {
+        const chapter = Number(chapterSelect.value);
+        const bookRef = Number(bookSelect.value);
+        if (bibleBook[bookRef] === null) {
+            alert('Could not find that chapter - make sure you have pressed the Open Book button first');
+        }
+        else {
+            updateChapter(bibleBook[bookRef], chapter - 1);
+            chapterSelect.value = String(chapter - 1);
+        }
     });
 })();
--- frontend/index.ts
+++ frontend/index.ts
@@ -31,22 +31,45 @@ function populateBookSelect() {
 }
 
 function updateChapter(book: string[], chapter: number) {
+    // Expects a human-readable chapter number from the caller, 
+    // which is immediately reset for the 0-indexed array
     chapter -= 1;
     const diffContainer = document.getElementById('diff-container');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
+    const prevChapterBtnTop = document.getElementById('nav-ctrl-prev-chapter-btn-top');
     if (diffContainer) {
         diffContainer.innerHTML = book[chapter];
-    }    
+    }
+    // Set disabled state of next/previous chapter buttons depending on what chapter we're on
+    console.log(`Current chapter index: ${chapter}`);
+    console.log(`Is next chapter in the book array? ... ${chapter+1 in book}`);
+    console.log(`Is previous chapter in the book array? ... ${chapter-1 in book}`);
+    if (chapter + 1 in book) {
+        nextChapterBtnTop?.removeAttribute('disabled');
+    }
+    else {
+        nextChapterBtnTop?.setAttribute('disabled', '');
+    }
+    if (chapter - 1 in book) {
+        prevChapterBtnTop?.removeAttribute('disabled');
+    }
+    else {
+        prevChapterBtnTop?.setAttribute('disabled', '');
+    }
 }
 
 function openBook(book: string[]) {
     // Updates the GUI to the selected book, including resetting chapter <select>'s child <option>s
+    // and enabling the relevant prev/next chapter controls
     // Defaults to showing chapter 1
     const chapterSelect = document.getElementById('nav-ctrl-chapter') as HTMLSelectElement;
     const navChapterSubmit = document.getElementById('nav-ctrl-chapter-submit');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
     chapterSelect.innerHTML = '';
     for (let i = 0; i < book.length; i++) {
         const option = document.createElement('option');
         option.appendChild(document.createTextNode(String(i+1)));
+        option.setAttribute('value', String(i+1));
         chapterSelect?.appendChild(option);
     }
     // Re-enable the chapter picker and submit button now it contains valid values
@@ -67,6 +90,8 @@ function openBook(book: string[]) {
     const chapterSelect = document.getElementById('nav-ctrl-chapter') as HTMLSelectElement;
     const navBookSubmit = document.getElementById('nav-ctrl-book-submit');
     const navChapterSubmit = document.getElementById('nav-ctrl-chapter-submit');
+    const nextChapterBtnTop = document.getElementById('nav-ctrl-next-chapter-btn-top');
+    const prevChapterBtnTop = document.getElementById('nav-ctrl-prev-chapter-btn-top');
 
     // Pull in book names and setup <select> with option values that correspond to JSON filenames
     // and bibleBook top-level array indicies
@@ -77,7 +102,7 @@ function openBook(book: string[]) {
 
         // If we haven't yet fetched the JSON file and stored internally, then do so...
         if (bibleBook[bookRef] === null) {
-            console.log(`bibleBook array at item ${bookRef} is null. Will download the relevant JSON file and store internally.`)
+            // console.log(`bibleBook array at item ${bookRef} is null. Will download the relevant JSON file and store internally.`)
             fetchJson(bookRef + '.json')
             .then((book) => {
                 openBook(book);
@@ -89,7 +114,7 @@ function openBook(book: string[]) {
         }
         // else we have that book already stored in memory that we can use
         else {
-            console.log(`bibleBook array at item ${bookRef} already contains our JSON object.`);
+            // console.log(`bibleBook array at item ${bookRef} already contains our JSON object.`);
             openBook(bibleBook[bookRef]);
         }        
     })
@@ -101,8 +126,8 @@ function openBook(book: string[]) {
             alert('Could not find that chapter - make sure you have pressed the Open Book button first');
         }
         else {
-            console.log(`Chapter: ${chapter}, bookRef: ${bookRef}`)
-            console.log('book.length: ' + bibleBook[bookRef].length)
+            // console.log(`Chapter: ${chapter}, bookRef: ${bookRef}`)
+            // console.log('book.length: ' + bibleBook[bookRef].length)
             updateChapter(bibleBook[bookRef], chapter);
         }
     });
@@ -110,9 +135,35 @@ function openBook(book: string[]) {
     bookSelect.addEventListener('change', () => {
         // Once the user updates the book <select> there's no way to guarantee we won't get index
         // out-of-range issues on the chapter picker, so it should be disabled until the new book
-        // is loaded
+        // is loaded. Same logic applies to all chapter navigation buttons.
         chapterSelect.setAttribute('disabled', '');
         navChapterSubmit?.setAttribute('disabled', '');
+        nextChapterBtnTop?.setAttribute('disabled', '');
+        prevChapterBtnTop?.setAttribute('disabled', '');
+    })
+
+    nextChapterBtnTop?.addEventListener('click', () => {
+        const chapter = Number(chapterSelect.value);
+        const bookRef = Number(bookSelect.value);
+        if (bibleBook[bookRef] === null) {
+            alert('Could not find that chapter - make sure you have pressed the Open Book button first');
+        }
+        else {
+            updateChapter(bibleBook[bookRef], chapter + 1);
+            chapterSelect.value = String(chapter + 1);
+        }
+    })
+
+    prevChapterBtnTop?.addEventListener('click', () => {
+        const chapter = Number(chapterSelect.value);
+        const bookRef = Number(bookSelect.value);
+        if (bibleBook[bookRef] === null) {
+            alert('Could not find that chapter - make sure you have pressed the Open Book button first');
+        }
+        else {
+            updateChapter(bibleBook[bookRef], chapter - 1);
+            chapterSelect.value = String(chapter - 1);
+        }
     })
 
 })();
